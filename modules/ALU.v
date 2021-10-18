b4392251b56b341156af94a8b94553670aeaeb55
Ryan Kinney <ryan.kinney@maine.edu>
2021-10-18 11:20:26 -0400

    Swap the operands for shift instructions in the ALU

--- modules/ALU.v
+++ modules/ALU.v
@@ -11,6 +11,9 @@ B is the second operand
 ALU_OP is the ALU control signal
 ALU_OUT is the result of the operation
 
+Shift instructions use operand B for the data being shifted
+Operand A in shift instructions is the shift amount
+
 ALU_OP corresponds to the following operations:
  0 - 0000 --- nop or sll
  2 - 0010 --- srl
@@ -25,9 +28,9 @@ ALU_OP corresponds to the following operations:
 
 always @* begin
 	if(ALU_OP == 4'b0010) begin // srl
-		ALU_OUT = A >> B[4:0];
+		ALU_OUT = B >> A[4:0];
 	end else if(ALU_OP == 4'b0011) begin // sra
-		ALU_OUT = A >>> B[4:0];
+		ALU_OUT = B >>> A[4:0];
 	end else if(ALU_OP == 4'b0101) begin // slt
 		if(A < B) begin
 			ALU_OUT = 32'd1;
@@ -45,7 +48,7 @@ always @* begin
 	end else if(ALU_OP == 4'b1111) begin // nor
 		ALU_OUT = ~(A | B);
 	end else begin // nop or sll
-		ALU_OUT = A << B[4:0];
+		ALU_OUT = B << A[4:0];
 	end
 end
 
