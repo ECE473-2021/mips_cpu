3a140f67f5d228c68a32e6d2ea418af6d69f7e39
Ryan Kinney <ryan.kinney@maine.edu>
2021-10-18 15:38:35 -0400

    Create a skeleton and symbol file for the forwarding unit

---
+++ forwarding_unit.v
@@ -0,0 +1,15 @@
+// file forwarding_unit.v
+
+module forwarding_unit(
+	input wire ID_EX_RS,
+	input wire ID_EX_RT,
+	input wire EX_MEM_RD,
+	input wire MEM_WB_RD,
+	input wire EX_MEM_REGWRITE,
+	input wire MEM_WB_REGWRITE,
+	output wire ALU_A,
+	output wire ALU_B
+	);
+	
+	
+endmodule
