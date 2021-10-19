01e29df3c0772894abe806aae9ef5cb990dd2787
LFrancis23 <landyn.francis@maine.edu>
2021-10-19 19:31:56 -0400

    COMPILES! Both memory modules added, not done
    It now compiles, with both memory modules, just need to add MEM_WB, and make sure stuff is cleaned up

--- modules/Pipeline_Registers/EX_MEM.v
+++ modules/Pipeline_Registers/EX_MEM.v
@@ -5,8 +5,16 @@ module EX_MEM(
 	input wire [31:0] EX_D2,
 	input wire [4:0] EX_RD,
 	input wire [31:0] EX_ALUResult,
+	input wire EX_RegWrite,
+	input wire EX_MemToReg,
+	input wire EX_MEM_WEN,
+	input wire EX_MEM_REN,
 	input wire clock,
 	input wire reset,
+	output reg MEM_RegWrite,
+	output reg MEM_MemToReg,
+	output reg MEM_MEM_WEN,
+	output reg MEM_MEM_REN,
 	output reg [31:0] MEM_D1,
 	output reg [31:0] MEM_D2,
 	output reg [4:0] MEM_RD,
@@ -23,6 +31,11 @@ always @(posedge clock or posedge reset)	begin
 		MEM_D2 <= EX_D2;
 		MEM_RD <= EX_RD;
 		MEM_ALUResult <= EX_ALUResult;
+		MEM_MEM_REN <= EX_MEM_REN;
+		MEM_MEM_WEN <= EX_MEM_WEN;
+		MEM_MemToReg <= EX_MemToReg;
+		MEM_RegWrite <= EX_RegWrite;
+		
 	end
 end
 	
--- modules/Pipeline_Registers/ID_EX.v
+++ modules/Pipeline_Registers/ID_EX.v
@@ -2,9 +2,10 @@
 
 module ID_EX(
 	input wire [3:0] ID_ALUOp,
-	input wire [31:0] ID_RS,
-	input wire [31:0] ID_RT,
+	input wire [31:0] ID_D1,
+	input wire [31:0] ID_D2,
 	input wire [4:0] ID_RD,
+	input wire [4:0] ID_RT,
 	input wire ID_RegWrite,
 	input wire ID_MemToReg,
 	input wire ID_MEM_WEN,
@@ -14,22 +15,23 @@ module ID_EX(
 	input wire clock,
 	input wire reset,
 	output reg [3:0] EX_ALUOp,
-	output reg [31:0] EX_RS,
-	output reg [31:0] EX_RT,
+	output reg [31:0] EX_D1,
+	output reg [31:0] EX_D2,
 	output reg [4:0] EX_RD,
 	output reg EX_RegWrite,
 	output reg EX_MemToReg,
 	output reg EX_MEM_WEN,
 	output reg EX_MEM_REN,
 	output reg EX_ALUSrc,
+	output reg [4:0] EX_RT,
 	output reg EX_RegDst);
 	
 	
 	// write on the positive edge of the clock
 	always @(posedge clock or posedge reset) begin
 		if(reset) begin
-			EX_RS <= 32'd0;
-			EX_RT <= 32'd0;
+			EX_D1 <= 32'd0;
+			EX_D2 <= 32'd0;
 			EX_RD <= 5'd0;
 			
 		end else begin
@@ -40,9 +42,10 @@ module ID_EX(
 			EX_MemToReg <= ID_MemToReg;
 			EX_MEM_WEN <= ID_MEM_WEN;
 			EX_MEM_REN <= ID_MEM_REN;
-			EX_RS <= ID_RS;
-			EX_RT <= ID_RT;
+			EX_D1 <= ID_D1;
+			EX_D2 <= ID_D2;
 			EX_RD <= ID_RD;
+			EX_RT <= ID_RT;
 		end
 	end
 	
