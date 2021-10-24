9c471b63ecc4e19278abef1f30796e502259b602
Ryan Kinney <ryan.kinney@maine.edu>
2021-10-24 16:44:02 -0400

    Add a write_enable flag to the IF_ID register

--- modules/Pipeline_Registers/IF_ID.v
+++ modules/Pipeline_Registers/IF_ID.v
@@ -7,21 +7,28 @@
 	 the instruction decode stage
 */
 module IF_ID(
+	input wire IF_ID_Write, // signal for stalling the pipeline
+
 	input wire [31:0] IF_INSTR, // the fetched instruction
-	input wire [31:0] IF_PC, // the PC for branch instructions
-	input wire clock,
-	input wire reset, // the reset lines are for flushing the register
 	output reg [31:0] ID_INSTR,
-	output reg [31:0] ID_PC);
 	
-	// set INSTR to IF_IN on the rising edge of the clock
+	input wire [31:0] IF_PC, // the PC for branch and jump instructions
+	output reg [31:0] ID_PC,
+	
+	input wire clock, // the clock and reset lines
+	input wire reset);
+
 	always @(posedge clock or posedge reset) begin
 		if(reset) begin
 			// reset; flush the registers
 			ID_INSTR <= 32'd0;
 			ID_PC <= 32'd0;
-		end else begin
+		end else if (!IF_ID_Write) begin
 			// write the new values on positive clock edge
+			ID_INSTR <= IF_INSTR;
+			ID_PC <= IF_PC;
+		end else begin
+			// we are stalling; do not update the values
 			ID_INSTR <= IF_INSTR;
 			ID_PC <= IF_PC;
 		end
