8240fee6a97a7de106a42ae8376d857dce086922
Jacob Mealey <jacob.mealey@maine.edu>
2021-11-02 18:57:43 -0400

    boom another fix (the last commit was broken :D )

--- modules/hazard_unit.v
+++ modules/hazard_unit.v
@@ -11,10 +11,10 @@
 	 to the ID/EX register.
 */
 
-module hazard_unit(IDEX_memread, IDEX_regiser_rs, IFID_register_rs, IDEX_regiser_rt, IFID_register_rt, stall);
+module hazard_unit(IDEX_memread, IDEX_register_rs, IFID_register_rs, IDEX_register_rt, IFID_register_rt, stall);
 	input wire IDEX_memread;
-	input wire [4:0] IDEX_regiser_rs;
-	input wire [4:0] IDEX_regiser_rt;
+	input wire [4:0] IDEX_register_rs;
+	input wire [4:0] IDEX_register_rt;
 	input wire [4:0] IFID_register_rs;
 	input wire [4:0] IFID_register_rt;
 	
@@ -24,7 +24,7 @@ module hazard_unit(IDEX_memread, IDEX_regiser_rs, IFID_register_rs, IDEX_regiser
 	// incase we have to add more to it in the future it will 
 	// good to have it? 
 	always @* begin
-		stall = IDEX_memread && ((IDEX_regiser_rt == IFID_register_rs) || (IDEX_regiser_rt == IFID_register_rt));
+		stall = IDEX_memread && ((IDEX_register_rt == IFID_register_rs) || (IDEX_register_rt == IFID_register_rt));
 	end
 	
 endmodule
